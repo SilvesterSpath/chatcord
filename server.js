4ffbe47a5f6408fc078d49cfde42b0d0e5dd63d9
SilvesterSpath <spath.sz@gmail.com>
2023-11-28 10:30:28 +0100

    set static folder

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
