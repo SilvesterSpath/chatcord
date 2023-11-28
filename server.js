a94103c78d800de21771371ee309a96eca2a456e
SilvesterSpath <spath.sz@gmail.com>
2023-11-28 10:39:25 +0100

    connecting socket.io

--- server.js
+++ server.js
@@ -2,17 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 dotenv.config({ path: './config/.env' });
 
+// Initialize
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.use(bodyParser.json());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when client connects
+io.on('connection', (socket) => {
+  console.log('New websocket connection..');
+});
+
+// Run server
 const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
