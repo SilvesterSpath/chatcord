f83a2ae9c2b03076fd5fa2f90e3cfa20f766c1a7
SilvesterSpath <spath.sz@gmail.com>
2023-11-28 11:13:20 +0100

    connecting the form

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 // Join chatroom (this message will be in the browser's console)
 socket.on('message', (message) => {
   console.log(message);
+});
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  console.log(msg);
 });
