57330313441a647901a267551d2221f41093be86
SilvesterSpath <spath.sz@gmail.com>
2023-11-29 10:20:27 +0100

    leave room functionality

--- public/js/main.js
+++ public/js/main.js
@@ -84,3 +84,12 @@ function outputUsers(users) {
 function outputRooms(room) {
   roomName.innerText = room;
 }
+
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
